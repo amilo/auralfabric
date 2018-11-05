c44e068e900f1584cd7e68ed492b526412e62312
amilo <miloalessia@gmail.com>
2018-11-05 16:47:38 +0000

    added aural fabric files ext

---
+++ build/SampleStream.d
@@ -0,0 +1,23 @@
+/root/Bela/projects/aural-fabric/build/SampleStream.o: \
+  /root/Bela/projects/aural-fabric/SampleStream.cpp \
+  /root/Bela/projects/aural-fabric/SampleStream.h \
+  /root/Bela/projects/aural-fabric/SampleData.h include/Bela.h \
+  /usr/xenomai/include/posix/unistd.h /usr/xenomai/include/posix/stdio.h \
+  include/digital_gpio_mapping.h include/GPIOcontrol.h \
+  include/Utilities.h
+
+/root/Bela/projects/aural-fabric/SampleStream.h:
+
+/root/Bela/projects/aural-fabric/SampleData.h:
+
+include/Bela.h:
+
+/usr/xenomai/include/posix/unistd.h:
+
+/usr/xenomai/include/posix/stdio.h:
+
+include/digital_gpio_mapping.h:
+
+include/GPIOcontrol.h:
+
+include/Utilities.h:
